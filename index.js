f05f23795d2bc00b0d318deaed9a4e1513b99ee8
Carla Valle <carla.valle@engenharia.ufjf.br>
2022-08-27 15:13:33 -0300

    feat: server lets you tweet

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(cors());
 app.use(express.json());
 
 const users = [];
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
     const user = req.body;
@@ -16,6 +17,16 @@ app.post("/sign-up", (req, res) => {
 app.get("/sign-up", (req, res) => {
     res.send(users);
 });
+
+app.post("/tweets", (req, res) => {
+    const tweet = req.body;
+    tweets.push(tweet);
+    res.send(tweet);
+})
+
+app.get("/tweets", (req, res) => {
+    res.send(tweets);
+})
 
 app.listen(5000, () => {
     console.log("Servidor rodando.")}
